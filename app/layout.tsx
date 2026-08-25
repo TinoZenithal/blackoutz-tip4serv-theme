8d6384053cefa5d10cc9c23e97bdccd78188a333
TinoZenithal <dylansciortino17@gmail.com>
2026-08-25 18:40:43 +1000

    Hand checkout to Tip4Serv

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const geistSans = Geist({
@@ -54,7 +55,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script src="https://js.tip4serv.com/tip4serv.min.js?v=1.0.16" data-store-id="21207" strategy="afterInteractive" />
       </body>
     </html>
   );
 }
+
